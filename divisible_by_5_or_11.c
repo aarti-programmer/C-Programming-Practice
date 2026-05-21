598a5b39138b33ef422a01d89815df7ef7e8d03a
Aarti Kushwah <aaru47527@gmail.com>
2026-05-21 11:59:56 +0530

    number is divisible by both 5 and 11

---
+++ divisible_by_5_or_11.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main () {
+    int num;
+    printf("Enter the number:\n");
+    scanf("%d", &num);
+
+    if (num % 5 == 0 && num % 11 == 0) {
+        printf("%d is divisible by 5 or 11", num);
+    } else {
+        printf("%d is not divisible by 5 or 11", num);
+    }
+    return 0;
+}
